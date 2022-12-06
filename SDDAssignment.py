57f3015dc2143df7605a47a388d0728020c6fbab
Joeygjh <Joeykkhgjh@gmail.com>
2022-12-06 13:52:24 +0800

    Added Display main menu Feature

---
+++ SDDAssignment.py
@@ -0,0 +1,14 @@
+import random
+#Display Main Menu - Joey
+def DisplayMainMenu():
+    print('Welcome, mayor of Ngee Ann City!')
+    print('------------------------------')
+    print('1. Start new game')
+    print('2. Load saved game')
+    print('3. Show high scores')
+    print()
+    print('0. Exit')
+    option = input('Your choice? ')
+    return option
+Part1 = DisplayMainMenu()
+
